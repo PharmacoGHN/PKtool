a411932d5d721ce6e8ad7a0999b444876e172b8d
Romain Garreau <romain.garreau@chu-lyon.fr>
2024-08-07 21:54:23 +0200

    auto_read function and test
    - get file extension
    - get separator if csv file (default is comma)
    - chose the function to read file.
    - replace file_indentification, deprecated and suppressed in this commit.
    - pmetrics-to-monolix deprecated and superseeded by p2m

--- R/pmetrics-to-monolix.R
+++ R/pmetrics-to-monolix.R
@@ -3,6 +3,7 @@
 #' @description
 #' pm2monolix is a function that translate xlsx or csv file used by pmetrics
 #' to lixoft format (Monolix and other software)
+#' Deprecated.
 #'
 #' @param file correspond to the input file
 #' @param admin_route correspond to the administration route
@@ -13,52 +14,50 @@
 #' @export
 
 pm2monolix <- function(
-   file,
-   admin_route = TRUE,
-   filename = "pm2monolix.csv",
-   pm_vers = 2
-) {
+    file,
+    admin_route = TRUE,
+    filename = "pm2monolix.csv",
+    pm_vers = 2) {
+  if (!stringr::str_ends(file, ".csv")) {
+    rlang::abort(message = "Error : the input file should be a .csv file")
+  }
 
-   if (!stringr::str_ends(file, ".csv")) {
-      rlang::abort(message = "Error : the input file should be a .csv file")
-   }
-
-   # loading instruction for pmetrics datafile.
-   if (stringr::str_ends(file, ".csv")) {
-      df <- readr::read_csv(file, skip = 1)
-   }
+  # loading instruction for pmetrics datafile.
+  if (stringr::str_ends(file, ".csv")) {
+    df <- readr::read_csv(file, skip = 1)
+  }
 
 
 
-   # re order data file to match monolix design
-   ## modulate dataset according to the administration route
-   ##  if no iv data, remove duration
-   # (function call iv = FALSE, by default iv = TRUE)
-   ## rename column
+  # re order data file to match monolix design
+  ## modulate dataset according to the administration route
+  ##  if no iv data, remove duration
+  # (function call iv = FALSE, by default iv = TRUE)
+  ## rename column
 
-   if (admin_route == TRUE) {
+  if (admin_route == TRUE) {
+    # column type
+    df_translated <-
+      cbind(df[1], df$TIME, df$DOSE, df$OUT, df$EVID, df$DUR, df$ADDL, df$II)
+    # ,
+    #          df[, 15:length(df[1, ])]) # covariates column
+    # column names
+    names(df_translated)[1:8] <-
+      c("ID", "TIME", "AMOUNT", "CONC", "EVENT ID", "TINF", "ADDL", "II")
+  } else {
+    # column types
+    df_translated <-
+      cbind(
+        df[1], df$TIME, df$DOSE, df$OUT, df$EVID, df$ADDL, df$II,
+        df[, 15:length(df[1, ])]
+      ) # covariates column
+    # column names
+    names(df_translated)[1:7] <-
+      c("ID", "TIME", "AMOUNT", "CONC", "EVENT ID", "ADDL", "II")
+  }
 
-      # column type
-      df_translated <-
-         cbind(df[1], df$TIME, df$DOSE, df$OUT, df$EVID, df$DUR, df$ADDL, df$II)
-      # ,
-      #          df[, 15:length(df[1, ])]) # covariates column
-      # column names
-      names(df_translated)[1:8] <-
-         c("ID", "TIME", "AMOUNT", "CONC", "EVENT ID", "TINF", "ADDL", "II")
+  return(df_translated)
 
-   } else {
-      # column types
-      df_translated <-
-         cbind(df[1], df$TIME, df$DOSE, df$OUT, df$EVID, df$ADDL, df$II,
-               df[, 15:length(df[1, ])]) # covariates column
-      # column names
-      names(df_translated)[1:7] <-
-         c("ID", "TIME", "AMOUNT", "CONC", "EVENT ID", "ADDL", "II")
-   }
-
-   return(df_translated)
-
-   # manage several output (such as Parent and metabolites)
-   # if (metabolite = TRUE)
+  # manage several output (such as Parent and metabolites)
+  # if (metabolite = TRUE)
 }
--- R/utils.R
+++ R/utils.R
@@ -13,6 +13,41 @@ NULL
 #'
 #' @export
 
-not_in <- function(a, b)  {
-   ! a %in% b
+not_in <- function(a, b) {
+  !a %in% b
+}
+
+
+#' auto_read
+#'
+#' get the file extension and the separator values and call the adequate
+#' function to read file.
+#'
+#' @noRd
+
+auto_read <- function(file, sep = ",", ...) {
+  file_extension <- tools::file_ext(file)
+
+  # check file extension and sperator are valid.
+  if (not_in(file_extension, c("csv", "xls", "xlsx"))) {
+    stop("your file must be a csv or an excel (xls or xlsx).")
+  }
+
+  if (not_in(sep, c(",", ";", "/t"))) {
+    stop("In case of csv separator must be '/t', ';' or ','")
+  }
+
+  read_extension <- dplyr::case_when(
+    file_extension == "csv" & sep == "," ~ "read.csv",
+    file_extension == "csv" & sep == ";" ~ "read.csv2",
+    .default = "read_excel"
+  )
+
+  if (file_extension == "csv") {
+    file <- get(read_extension)(file, sep, header = TRUE, ...)
+  } else {
+    file <- get(read_extension, envir = asNamespace("readxl"))(file, ...)
+  }
+
+  return(file)
 }
