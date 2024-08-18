df6388cf2c6220f7762779404fd6ebe615d4c8ab
Romain Garreau <romain.garreau@chu-lyon.fr>
2024-08-18 22:19:47 +0200

    create function skeleton

--- R/pksim_translate.R
+++ R/pksim_translate.R
@@ -25,45 +25,7 @@ psim2pm <- function(
     molecule = NULL,
     s_id = NULL,
     file = "Translate2pksim.csv") {
-  # loading instruction for pmetrics datafile.
-  df <- readr::read_csv(x, skip = 1)
 
-
-  # re order data file to match pksim design
-  ## rename column
-
-  if (sex == 0) {
-    # column type
-    df_pksim <- cbind(df[3], df$OUT, df["new1"] <- NA, df["new2"] <- NA, df$EVID, df["Study ID"] <- NA, df["new3"] <- NA)
-
-    # column names
-    names(df_pksim)[1:7] <- c(
-      paste0("Time [", time, "]"), paste0("Concentration [", unit, "]"),
-      "Tissue", "Compartment", "ID", "Study ID", "Molecule"
-    )
-  } else {
-    # column type
-    df_pksim <- cbind(df$TIME, df$OUT, df["new1"] <- NA, df["new2"] <- NA, df$EVID, df["Study ID"] <- NA, df["new3"] <- NA, df[sex])
-
-    # column names
-    names(df_pksim)[c(1:8)] <- c(
-      paste0("Time [", time, "]"), paste0("Concentration [", unit, "]"),
-      "Tissue", "Compartment", "ID", "Study ID", "Molecule", "Sex"
-    )
-  }
-
-  # manage several output (such as Parent and metabolites)
-  # if (metabolite = TRUE)
-  df_pksim["Molecule"] <- molecule # Fill molecule column
-
-  # Row fill according to variable called in the argument
-  df_pksim["Tissue"] <- tissue # Fill tissue for all observation
-  df_pksim["Compartment"] <- compartment # Fill compartment for all observation
-  df_pksim["Study ID"] <- s_id # Fill study ID
-
-
-  # save object into csv file.
-  write.csv2(df_pksim, file = file, row.names = FALSE, sep = ",", dec = ".")
 }
 
 
--- R/read_mrgsolve.R
+++ R/read_mrgsolve.R
@@ -2,6 +2,15 @@
 #
 # This is a set of function designed to read NONMEM/mrgsolve type file and perform certain check before
 # Changing the data into another format.
+# 
+#
+# In addition the function will check EVID values if present as only the following values are accepted.
+# 0 = observation record
+# 1 = dosing event (bolus or infusion)
+# 2 = other type event, with solver stop and restart
+# 3 = system reset
+# 4 = reset and dose
+# 8 = replace the amount in the compartment with amt
 
 
 #' mrg_check_colname
@@ -14,14 +23,37 @@
 #' @noRd
 
 mrg_check_colname <- function(data) {
-  data <- case_colname(data, "upper")
-  expecte_mrg_colnames <- c("ID", "TIME", "EVID", "AMT", "CMT", "RATE", "II", "ADDL", "SS")
 
-  # check EVID if present. Only these values are accepted.
-  # 0 = observation record
-  # 1 = dosing event (bolus or infusion)
-  # 2 = other type event, with solver stop and restart
-  # 3 = system reset
-  # 4 = reset and dose
-  # 8 = replace the amount in the compartment with amt
+  data <- case_colname(data, "upper")
+  expecte_mrg_colnames <- c("ID", "TIME", "EVID", "AMT", "CMT", "RATE", "II", "ADDL", "SS", "CONC")
+  mrg_colnames <- colnames(data)
+
+
+  # extract existing variable *var_name*
+  var_pos <- which(mrg_colnames %in% expected_mrg_colnames)
+  var_names <- mrg_colnames[var_pos]
+  var_missing <- expected_mrg_colnames[which(not_in(expected_mrg_colnames, mrg_colnames))]
+
+  # check if the 5 mandatory variable are present. NB : the check is not case sensitive
+  mandatory_pm_var <- c("ID", "TIME", "AMT", "CONC", "EVID")
+  is_mandatory <- ifelse(all(mandatory_pm_var %in% var_names), TRUE, FALSE)
+
+  # check if evid contain the right value
+  if ("EVID" %in% mrg_colnames) evid_check <- ifelse(all(data$EVID %in% c("0", "1", "2", "3", "4", "8")), TRUE, FALSE)
+  if (not_in("EVID", mrg_colnames)) evid_check <- FALSE
+  
+  # check if covariate are present and get their position
+  cov_pos <- which(not_in(mrg_colnames, expected_mrg_colnames))
+  cov_name <- mrg_colnames[cov_pos]
+
+  # check if the data is complete (all expected colnames are present)
+  is_complete <- ifelse(all(expected_mrg_colnames %in% mrg_colnames), TRUE, FALSE)
+
+  return(
+    list(
+      check = list(mandatory = is_mandatory, complete = is_complete, evid = evid_check),
+      var = list(name = var_names, pos = var_pos, missing = var_missing),
+      covar = list(name = cov_name, pos = cov_pos)
+    )
+  )
 }
