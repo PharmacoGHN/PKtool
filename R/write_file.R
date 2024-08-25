43d81643ca830903e0797f23039e5393a0dee100
Romain Garreau <romain.garreau@chu-lyon.fr>
2024-08-25 18:46:44 +0200

    update write function

--- R/write_file.R
+++ R/write_file.R
@@ -1,22 +1,37 @@
 #' @title write_file
+#' 
+#' @description
+#' this function create a file corresponding to the software for which the file was translated.
+#' Only Pmetrics, Lixoft suite, PKsim and NONMEM type are supported. Software and extebsion type
+#' are automatically retrvied from the object which must have been translated with the PKtool function.
+#' Extention type is retrieved directly from the path given as the user must specify the path to save
+#' the file by giving it name and extensiton ex : filename = "C:/newdata.csv" 
 #'
 #' @param object is the object supplied to write the file. Must be a dataframe or tibble
 #' @param filename is the name the file should have
-#' @param software correspond to the software for which the file is adapted. Only Pmetrics, Lixoft suite, PKsim and NONMEM are supported
-#' @param ext dictate the type of file created
-#' (only file supported by pharmacometrics software are possible)
+#' @param ... dictate the type of file created (only file supported by pharmacometrics software are possible)
+#' 
+#' @author Romain Garreau
+#' @export 
 
 write_file <- function(
-    object,
-    filename,
-    software = c("Pmetrics", "Lixoft", "PKsim", "NONMEM"),
-    ext = c("csv", "xlsx", "xls", "tsv")) {
-  # check for error
-  if (length(software) > 1 || length(ext) > 1) stop("Error : please check that only one object was passed to software and ext argument")
+    object = NULL,
+    filename = NULL, 
+    ...
+    ) {
+  if(is.null(object)) stop("Error: there is no object to save.")
+  if(is.null(filename)) stop("Error: give a the path where to save the file.")
+  
+  # check the extension passed is correct
+  file_ext = tools::file_ext(filename)
+  if (not_in(file_ext, c("csv", "xlsx", "xls", "tsv"))) stop("This file extension is not supported. Please select file among csv, xlsx, xls or tsv")
+  
+  # check for software type
+  software <- attributes(object)$software
   if (not_in(software, c("Pmetrics", "Lixoft", "PKsim", "NONMEM"))) stop("Please select a software from Pmetrics, Lixoft, PKsim, or NONMEM. The other software are not supported")
-  if (not_in(ext, c("csv", "xlsx", "xls", "tsv"))) stop("This file extension is not supported. Please select file among csv, xlsx, xls or tsv")
-
+    
+  
   if (software == "Pmetrics") {
-    utils::write.csv2(object, file = file, row.names = FALSE, sep = ",")
+    utils::write.csv(object, file = filename, row.names = FALSE, sep = ",")
   }
 }
--- tests/testthat/test-write_file.R
+++ tests/testthat/test-write_file.R
@@ -1,5 +1,9 @@
 testthat::test_that("return error if argument not in the list", {
-   testthat::expect_error(write_file(software = "Phoenix", ext = "csv"), "Please select a software from Pmetrics, Lixoft, PKsim, or NONMEM. The other software are not supported")
-   testthat::expect_error(write_file(software = "Pmetrics", ext = "xml"), "This file extension is not supported. Please select file among csv, xlsx, xls or tsv")
-   testthat::expect_error(write_file(), "Error : please check that only one object was passed to software and ext argument")
+   test_data <- data.frame(id = c(1,2), id2 = c(1,2))
+   attr(test_data, "software") <- "none"
+   #testthat::expect_error(write_file(software = "Pmetrics", ext = "xml"), "This file extension is not supported. Please select file among csv, xlsx, xls or tsv")
+   testthat::expect_error(write_file(), "Error: there is no object to save.")
+   testthat::expect_error(write_file("test"), "Error: give a the path where to save the file.")
+   testthat::expect_error(write_file(test_data, "filename.ext"), "This file extension is not supported. Please select file among csv, xlsx, xls or tsv", fixed = TRUE)
+   testthat::expect_error(write_file(test_data, "filename.csv"), "Please select a software from Pmetrics, Lixoft, PKsim, or NONMEM. The other software are not supported")
 })
